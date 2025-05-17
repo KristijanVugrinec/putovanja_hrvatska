7cacb6581e27b9c28685c23174e3f4623676ceaf
Kristijan Vugrinec <kristijan.vugrinec2@gmail.com>
2025-05-17 14:18:26 +0200

    imported components

--- app/page.js
+++ app/page.js
@@ -3,12 +3,16 @@ import styles from "./page.module.css";
 import Header from "./header";
 import VideoAbout from './videoabout'
 import Counter from "./counter";
+import Card from "./cards";
+import Festivals from "./festivals";
 
 export default function Home() {
   return (
     <div>
       <VideoAbout />
       <Counter />
+      <Card />
+      <Festivals />
     </div>
   );
 }
